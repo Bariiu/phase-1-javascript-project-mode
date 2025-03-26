28aaba2568dd47794a6d932e6dcad28b900a9b43
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-03-26 15:10:59 +0300

    js file update

---
+++ js/script.js
@@ -0,0 +1,21 @@
+fetch('db.json')
+    .then(response => response.json())
+    .then(data => {
+        let servicesHTML = "<ul>";
+        data.services.forEach(service => {
+            servicesHTML += `<li>${service}</li>`;
+        });
+        servicesHTML += "</ul>";
+        document.getElementById("services-list").innerHTML = servicesHTML;
+        let doctorsHTML = "";
+        data.doctors.forEach(doctor => {
+            doctorsHTML += `
+                <div class="doctor-card">
+                    <h3>${doctor.name}</h3>
+                    <p>Specialty: ${doctor.specialty}</p>
+                </div>
+            `;
+        });
+        document.getElementById("doctors-list").innerHTML = doctorsHTML;
+    })
+    .catch(error => console.error('Error loading data:', error));
